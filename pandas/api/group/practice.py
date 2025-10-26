f27fd83c36ef7b3b2e6f8761f3bacb513e51462b
maeng0830 <kmk930830@gmail.com>
2025-10-26 20:07:21 +0900

    feat: study group_and_aggregation3

--- pandas/api/group/practice.py
+++ pandas/api/group/practice.py
@@ -22,3 +22,46 @@ a_df['big_or_small'] = a_df['시가총액 (보통)(평균)(원)'] >= median_df['
 a_df.loc[a_df['big_or_small'], 'big_or_small'] = '대형주'
 a_df.loc[a_df['big_or_small'] == False, 'big_or_small'] = '소형주'
 print(a_df.head(2))
+
+###################################################################
+# add Data using loc -> inplace=True
+df = pd.DataFrame(columns=['a', 'b'])
+print(df)
+# Empty DataFrame
+# Columns: [a, b]
+# Index: []
+
+## add Data using list
+df.loc[0] = [1, 2]
+print(df)
+#    a  b
+# 0  1  2
+
+df.loc['z'] = [1, 2]
+print(df)
+#    a  b
+# 0  1  2
+# z  1  2
+
+## add Data using dict
+df.loc[len(df)] = {'a': 3, 'b': 4}
+print(df)
+#    a  b
+# 0  1  2
+# z  1  2
+# 2  3  4
+
+## add Data using Series
+add_series = pd.Series({'a': 5, 'b': 6})
+print(add_series)
+# a    5
+# b    6
+# dtype: int64
+
+df.loc['x'] = add_series
+print(df)
+#    a  b
+# 0  1  2
+# z  1  2
+# 2  3  4
+# x  5  6
